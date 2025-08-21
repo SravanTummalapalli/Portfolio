1f72f594f98bc4726e962f0506ca42fdfd1aa9f5
SravanTummalapalli <sravantummalapalli@gmail.com>
2025-08-21 20:03:44 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(async ({ mode }) => {
-  // dynamically import lovable-tagger (ESM only)
-  const { componentTagger } = await import("lovable-tagger");
-
-  return {
-    server: {
-      host: "::",
-      port: 8080,
-    },
-    plugins: [
-      react(),
-      mode === "development" && componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
